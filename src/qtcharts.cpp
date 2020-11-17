9947349134a0025e622d1dac943b732e5cb9ae1e
hcasse <casse@irit.fr>
2020-11-17 07:56:35 +0100

    Improved interactions with qtcharts.

--- src/qtcharts.cpp
+++ src/qtcharts.cpp
@@ -11,8 +11,11 @@
 #include <QChartView>
 #include <QMainWindow>
 #include <QValueAxis>
+#include <QGestureEvent>
 
 namespace physim {
+
+using namespace QtCharts;
 
 static QColor colors[] = {
 	QColor(Qt::black),
@@ -27,6 +30,105 @@ static QColor colors[] = {
 	QColor(Qt::darkMagenta)
 };
 static const int color_count = sizeof(colors) / sizeof(QColor);
+
+static int argc = 1;
+static char app_name[] = "qtcharts";
+static char *argv[] = { app_name, nullptr };
+
+class Application: public QApplication {
+public:
+	Application(): QApplication(argc, argv) {
+	}
+};
+
+class Chart : public QtCharts::QChart {
+public:
+	bool sceneEvent(QEvent *event) {
+		if (event->type() == QEvent::Gesture)
+			return gestureEvent(static_cast<QGestureEvent *>(event));
+		return QChart::event(event);
+	}
+
+	bool gestureEvent(QGestureEvent *event) {
+		if(QGesture *gesture = event->gesture(Qt::PanGesture)) {
+			QPanGesture *pan = static_cast<QPanGesture *>(gesture);
+			QChart::scroll(-(pan->delta().x()), pan->delta().y());
+		}
+		if (QGesture *gesture = event->gesture(Qt::PinchGesture)) {
+			QPinchGesture *pinch = static_cast<QPinchGesture *>(gesture);
+			if (pinch->changeFlags() & QPinchGesture::ScaleFactorChanged)
+				QChart::zoom(pinch->scaleFactor());
+		}
+		return true;
+	}
+};
+
+class LineChartView: public QtCharts::QChartView {
+public:
+	LineChartView(QtCharts::QChart *chart): QtCharts::QChartView(chart) { }
+protected:
+
+	void keyPressEvent(QKeyEvent *event) {
+		switch (event->key()) {
+		case Qt::Key_Plus:
+			chart()->zoomIn();
+			break;
+		case Qt::Key_Minus:
+			chart()->zoomOut();
+			break;
+		}
+	}
+
+
+	void mousePressEvent(QMouseEvent *event) {
+		if(event->button() == Qt::LeftButton) {
+			QApplication::setOverrideCursor(QCursor(Qt::SizeAllCursor));
+			last_pos = event->pos();
+			event->accept();
+		}
+		QChartView::mousePressEvent(event);
+	}
+
+	void mouseMoveEvent(QMouseEvent *event) {
+		if(event->buttons() & Qt::LeftButton) {
+			auto s = event->pos() - last_pos;
+			chart()->scroll(-s.x(), s.y());
+			last_pos = event->pos();
+			event->accept();
+		}
+		QChartView::mouseMoveEvent(event);
+	}
+
+	void wheelEvent(QWheelEvent *event) {
+		qreal factor = event->angleDelta().y() > 0? 0.75: 1.5;
+		chart()->zoom(factor);
+		event->accept();
+		QChartView::wheelEvent(event);
+	}
+
+private:
+	QPointF last_pos;
+};
+
+class LineWindow: public QMainWindow {
+public:
+	LineWindow(Application& application, Chart *chart):
+		app(application), view(chart)
+	{
+		view.setRenderHint(QPainter::Antialiasing);
+		setCentralWidget(&view);
+		resize(820, 600);
+	}
+
+	void run() {
+		show();
+		app.exec();
+	}
+
+private:
+	LineChartView view;
+	Application& app;
+};
 
 
 /**
@@ -70,14 +172,14 @@ void QtLineDisplay::start() {
 
 ///
 void QtLineDisplay::stop() {
-	//QtCharts::QChartView view;
-	int argc = 1;
+	Application app;
+	/*int argc = 1;
 	char t[] = "";
 	char *argv[] = { t };
-	QApplication app(argc, argv);
+	QApplication app(argc, argv);*/
 
 	// build the chart
-	auto chart = new QtCharts::QChart();
+	auto chart = new Chart();
 	for(auto r: _reps) {
 		auto ls = new QtCharts::QLineSeries();
 		for(int i = 0; i < _dates.size(); i++)
@@ -91,13 +193,15 @@ void QtLineDisplay::stop() {
 	chart->createDefaultAxes();
 
 	// display it
-	auto view = new QtCharts::QChartView(chart);
-	view->setRenderHint(QPainter::Antialiasing);
+	auto view = new LineChartView(chart);
+	/*view->setRenderHint(QPainter::Antialiasing);
 	QMainWindow window;
 	window.setCentralWidget(view);
 	window.resize(820, 600);
 	window.show();
-	app.exec();
+	app.exec();*/
+	LineWindow win(app, chart);
+	win.run();
 }
 
 ///
